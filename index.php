98e392c2f15d5777ccfc2adc2b00f2b902f9211a
Said Robley <saidrobley@gmail.com>
2018-02-19 18:11:57 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Said Robley | Web Developer</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Marck+Script|Open+Sans|Pinyon+Script" rel="stylesheet">
+		<link href="images/logo.png" rel="shortcut icon" type="image/x-icon" />
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#"><img src="images/logo.png"></a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="http://saidrobley.com/index">Home</a></li>
+						<li><a href="#About">About</a></li>
+						<li><a href="#Skills">Skills</a></li>
+         				<li><a href="#Projects">Projects</a></li>
+						<li><a href="#Contact">Contact</a></li>	
+					</ul>
+					
+				</div>
+
+			</div>
+
+		</nav> <!-- End of navbar -->
+
+		<!-- ************************ HEADER SECTION ******************** -->
+		<header>
+			<div class="header-img">
+				<img class="img-responsive" src="images/main-image2.png" alt="">
+			</div>
+		
+		</header>
+		
+		<!-- ************************ ABOUT ME SECTION ******************** -->
+		<div class="about-me container-fluid id="About"">
+			<div class="row">
+				<h1 class="col-md-12 col-sm-12 col-xs-12 text-center">About Me</h2>
+				
+				<!--	<h1 class="col-md-12 col-sm-12 col-xs-12 text-center">--------</h1> -->
+				
+				<div class="col-md-4 col-sm-6 col-xs-12 text-center">
+					<img class="img-responsive center-block" src="images/my-picture.png" alt="">
+				</div>
+				<div class="col-md-6 col-sm-6 col-xs-12 text-center">
+					<h3>Said Robley</h3>
+					<p>I am a senior student majoring in Computer Science. I have been using computer 
+						programming to solve real world problems, some of the languages/frameworks I have used in 
+						the past include Java, JavaScript, Node/Express, Ruby on Rails, React and more. 
+					</p>
+				</div>
+				
+			</div>
+			<hr>
+		</div>
+		
+		
+		<!-- ************************ MY SKILLS SECTION ******************** -->
+		<div class="my-skills container-fluid" id="Skills">
+			<div class="row">
+				<h1 class="col-md-12 col-sm-12 col-xs-12 text-center">My skills</h1>
+			</div>	
+			<div class="title-image row">
+				<div class="box col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center">
+					<h4>HTML5</h4>
+					<img class="img-responsive" src="images/html-image.png" alt="">
+				</div>
+				<div class="box col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center">
+					<h4>CSS3</h4>
+					<img class="img-responsive" src="images/css-image.png" alt="">
+				</div>
+				<div class="box col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center">
+					<h4>JQUERY</h4>
+					<img class="img-responsive" src="images/jquery-image.png" alt="">
+				</div>
+				<div class="box col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center">
+					<h4>JAVASCRIPT</h4>
+					<img class="img-responsive" src="images/js-image.png" alt="">
+				</div>
+				<div class="box col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center">
+					<h4>BOOTSTRAP</h4>
+					<img class="img-responsive" src="images/bootstrap-image.png" alt="">
+				</div>
+				
+				<div class="box col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center">
+					<h4>GIT</h4>
+					<img class="img-responsive " src="images/git-image.png" alt="">
+				</div>
+				<div class="box col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center">
+					<h4>GITHUB</h4>
+					<img class="img-responsive" src="images/github-image.png" alt="">
+				</div>
+				<div class="box col-xs-6 col-sm-4 col-md-3 col-lg-2 text-center">
+					<h4>REACT</h4>
+					<img class="img-responsive" src="images/react-image.png" alt="">
+				</div>	
+			</div>
+			<hr>
+		</div>
+	
+		<!-- ******************* PROJECTS SECTION ***************** -->
+		<div class="container-fluid my-projects" id="Projects">
+			<div class="row">
+			<h1 class="col-md-12 col-sm-12 col-xs-12 text-center">My Projects</h1>
+			</div>
+		</div>
+		
+
+		
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="js/app.js"></script>
+	</body>
+</html>
